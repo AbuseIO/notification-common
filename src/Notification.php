66bd4602cfd24a11c9c7e0a3ee6c03c5fcd73c44
Bart Vrancken <bart@abuse.io>
2015-11-08 20:58:40 +0100

    add class holder

---
+++ src/Notification.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AbuseIO\Notification;
+
+class Notification
+{
+    /**
+     * Create a new Notification instance
+     */
+    public function __construct()
+    {
+        //
+    }
+}
